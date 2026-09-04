e7b67195f8b471cedeb07841f9b52326cbb2c682
ZethanSmith1996 <30243765@blackpool.ac.uk>
2026-09-04 16:49:46 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,15 @@
-const CACHE_NAME = "merch-till-pwa-v13a";
+const CACHE_NAME = "merch-till-pwa-v13b";
 
 const APP_SHELL = [
     "./",
     "./index.html",
-    "./style.css?v=priority13a",
+    "./style.css?v=priority13b",
     "./manifest.webmanifest",
     "./icons/icon-180.png",
     "./icons/icon-192.png",
     "./icons/icon-512.png",
     "./icons/icon-512-maskable.png",
-    "./js/app.js?v=priority13a"
+    "./js/app.js?v=priority13b"
 ];
 
 self.addEventListener("install", function (event) {
