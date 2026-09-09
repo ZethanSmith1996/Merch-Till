cf53e852e3a30b783b7190783f9c92be520d5e05
ZethanSmith1996 <30243765@blackpool.ac.uk>
2026-09-09 12:51:54 +0100

    v19- added scrollable modal popups

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,15 @@
-const CACHE_NAME = "merch-till-pwa-v18b1";
+const CACHE_NAME = "merch-till-pwa-v19-5";
 
 const APP_SHELL = [
     "./",
     "./index.html",
-    "./style.css?v=stage18b1",
+    "./style.css?v=stage19-5",
     "./manifest.webmanifest",
     "./icons/icon-180.png",
     "./icons/icon-192.png",
     "./icons/icon-512.png",
     "./icons/icon-512-maskable.png",
-    "./js/app.js?v=stage18b1"
+    "./js/app.js?v=stage19-5"
 ];
 
 self.addEventListener("install", function (event) {
