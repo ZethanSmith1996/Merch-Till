ee7ec7b75babee16a7ddbcb7b55911027e4fb831
ZethanSmith1996 <30243765@blackpool.ac.uk>
2026-09-15 17:20:34 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ import {
 import {
     initialiseProductManagement,
     renderProductsTable
-} from "./products.js?v=stage24-1";
+} from "./products.js?v=stage20";
 import {
     initialiseTill,
     renderCart,
@@ -27,7 +27,7 @@ import {
 import {
     initialiseReports,
     renderReports
-} from "./reports.js?v=stage24-1";
+} from "./reports.js?v=priority13b3";
 import {
     initialiseUserManagement,
     renderUsersTable
@@ -48,11 +48,11 @@ import {
     getCurrentDepartment
 } from "./department-context.js?v=stage15f2";
 import { initialiseProductions, refreshCurrentProduction } from "./productions.js?v=stage15e1";
-import { initialiseArchive } from "./archive.js?v=stage24-1";
+import { initialiseArchive } from "./archive.js?v=stage23-4";
 import {
     initialiseAccount,
     refreshDiscountAuthorisers
-} from "./account.js?v=stage23-1";
+} from "./account.js?v=stage23-2";
 
 function refreshProductDisplays() {
     renderTillProducts();
